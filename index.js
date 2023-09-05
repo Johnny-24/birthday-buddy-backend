d436ae7e2c04aac21b3d95c3ca97427bd32eb717
Johnny <dos_24@inbox.ru>
2023-09-06 01:31:14 +0500

    add mysql

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const sequelize = require('./utils/database')
 const listRoutes = require('./routes/list')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use('/api/list', listRoutes)
 
-app.listen(PORT)
+async function start() {
+  try {
+    await sequelize.sync()
+    app.listen(PORT)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
